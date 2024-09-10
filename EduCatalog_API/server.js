3c160912871d0f5bb836206a76a17f930b8148e8
KobeJaimes <nikolaskadric.ns@gmail.com>
2024-09-10 19:45:49 +0200

    inital setup for bootcamps route

--- EduCatalog_API/server.js
+++ EduCatalog_API/server.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const logger = require("./middlewares/logger");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
+
+app.get("/", (req, res) => {
+  res.send("Hello from express");
+});
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Mount routers
+// for bootcamps it will autmatically use this route /api/v1/bootcamps
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5001;
 
