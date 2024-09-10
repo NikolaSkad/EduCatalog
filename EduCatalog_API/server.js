cf1af3ecdd3a4b3059690e05eef98baeac957034
KobeJaimes <nikolaskadric.ns@gmail.com>
2024-09-10 20:16:48 +0200

    add connection to mongo DB

--- EduCatalog_API/server.js
+++ EduCatalog_API/server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 const logger = require("./middlewares/logger");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
-const app = express();
+connectDB();
 
 // Route files
 const bootcamps = require("./routes/bootcamps");
+
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello from express");
@@ -23,9 +27,18 @@ if (process.env.NODE_ENV === "development") {
 // for bootcamps it will autmatically use this route /api/v1/bootcamps
 app.use("/api/v1/bootcamps", bootcamps);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
