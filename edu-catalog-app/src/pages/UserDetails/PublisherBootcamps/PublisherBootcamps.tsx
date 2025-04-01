dc1afef7b14e9ff29f563ac5c7134a881b966a26
KobeJaimes <nikolaskadric.ns@gmail.com>
2025-04-01 19:07:12 +0200

    hide section when there is no bootcamps

--- edu-catalog-app/src/pages/UserDetails/PublisherBootcamps/PublisherBootcamps.tsx
+++ edu-catalog-app/src/pages/UserDetails/PublisherBootcamps/PublisherBootcamps.tsx
@@ -7,14 +7,16 @@ const PublisherBootcamps = () => {
   const { data: bootcamps = [] } = useQuery({ queryKey: ['bootcamps-publisher'], queryFn: getBootcampsForPublisher });
 
   return (
-    <div>
-      <h2 className="card-title mb-4">Your bootcamps</h2>
-      <div className="max-w-[450px]">
-        {bootcamps.map((bootcamp) => (
-          <BootcampCard {...bootcamp} key={bootcamp._id} />
-        ))}
+    <ShowAt at={bootcamps.length > 0}>
+      <div>
+        <h2 className="card-title mb-4">Your bootcamps</h2>
+        <div className="max-w-[450px]">
+          {bootcamps.map((bootcamp) => (
+            <BootcampCard {...bootcamp} key={bootcamp._id} />
+          ))}
+        </div>
       </div>
-    </div>
+    </ShowAt>
   );
 };
 
