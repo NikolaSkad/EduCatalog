0fa5371dbe84c0b5dde19d2cee158a4e2bcf7630
KobeJaimes <nikolaskadric.ns@gmail.com>
2025-04-01 19:06:05 +0200

    fix no results

--- edu-catalog-app/src/pages/UserDetails/PublisherBootcamps/PublisherBootcamps.tsx
+++ edu-catalog-app/src/pages/UserDetails/PublisherBootcamps/PublisherBootcamps.tsx
@@ -1,16 +1,17 @@
 import BootcampCard from '@/components/BootcampCard/BootcampCard';
+import ShowAt from '@/components/ShowAt/ShowAt';
 import { getBootcampsForPublisher } from '@/services/bootcamp';
 import { useQuery } from '@tanstack/react-query';
 
 const PublisherBootcamps = () => {
-  const { data: bootcamps } = useQuery({ queryKey: ['bootcamps-publisher'], queryFn: getBootcampsForPublisher });
+  const { data: bootcamps = [] } = useQuery({ queryKey: ['bootcamps-publisher'], queryFn: getBootcampsForPublisher });
 
   return (
     <div>
       <h2 className="card-title mb-4">Your bootcamps</h2>
       <div className="max-w-[450px]">
         {bootcamps.map((bootcamp) => (
-          <BootcampCard {...bootcamp} />
+          <BootcampCard {...bootcamp} key={bootcamp._id} />
         ))}
       </div>
     </div>
