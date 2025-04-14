8dd4d02cb55be853dcb621397998c2e51ae1a4e6
KobeJaimes <nikolaskadric.ns@gmail.com>
2025-04-14 15:59:13 +0200

    add redirect if user is logged out and show publisher bootcamps only for publisher user

--- edu-catalog-app/src/pages/UserDetails/UserDetails.tsx
+++ edu-catalog-app/src/pages/UserDetails/UserDetails.tsx
@@ -1,12 +1,26 @@
-import ExpandableSection from '@/components/ExpandableSection/ExpandableSection';
 import { useAuthStore } from '@/store/auth';
 import EditUserInfo from './EditUserInfo/EditUserInfo';
 import UserInfo from './UserInfo/UserInfo';
 import EditPassword from './EditPassword/EditPassword';
 import PublisherBootcamps from './PublisherBootcamps/PublisherBootcamps';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const UserDetails = () => {
   const { userInfo } = useAuthStore();
+  const navigate = useNavigate();
+
+  if (!userInfo) {
+    navigate('/');
+  }
+
+  useEffect(() => {
+    if (!userInfo) {
+      navigate('/');
+    }
+  }, [userInfo, navigate]);
+
+  if (!userInfo) return null;
 
   return (
     <div className="flex justify-center min-h-screen bg-base-200 rounded-2xl">
@@ -14,7 +28,7 @@ const UserDetails = () => {
         <UserInfo {...userInfo} />
         <EditUserInfo />
         <EditPassword />
-        <PublisherBootcamps />
+        {userInfo.role === 'publisher' && <PublisherBootcamps />}
       </div>
     </div>
   );
