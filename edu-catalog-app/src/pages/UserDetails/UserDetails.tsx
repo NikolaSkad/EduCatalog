3ef0a49efece6adb8f0518f11f2ef8669be484ad
KobeJaimes <nikolaskadric.ns@gmail.com>
2025-04-14 16:52:01 +0200

    remove unused import

--- edu-catalog-app/src/pages/UserDetails/UserDetails.tsx
+++ edu-catalog-app/src/pages/UserDetails/UserDetails.tsx
@@ -5,7 +5,6 @@ import EditPassword from './EditPassword/EditPassword';
 import PublisherBootcamps from './PublisherBootcamps/PublisherBootcamps';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
-import AddBootcampForm from '../Bootcamp/AddBootcampForm/AddBootcampForm';
 
 const UserDetails = () => {
   const { userInfo } = useAuthStore();
