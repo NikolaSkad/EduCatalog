ac71b5d635c79118078e4ade57fd8f59aac8718d
KobeJaimes <nikolaskadric.ns@gmail.com>
2025-09-23 17:54:40 +0200

    .

--- EduCatalog_API/server.js
+++ EduCatalog_API/server.js
@@ -63,7 +63,7 @@ app.use(limiter);
 app.use(hpp());
 
 const corsOptions = {
-  origin: "http://localhost:5173", // Frontend origin
+  origin: ["http://localhost:5173", "http://localhost:5174"], // Frontend origins
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
   allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
   credentials: true, // Allow credentials (cookies, etc.)
